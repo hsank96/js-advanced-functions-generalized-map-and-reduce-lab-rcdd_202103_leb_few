fcac2f7645c72480b1720f821f9e5c5444f63477
Hazem El Sankari <hazemleb96@gmail.com>
2021-05-03 15:51:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Add your functions here
+// // Add your functions here
+
+const a1=[1, 2, 3, -9];
+
+function f1_negative(a){
+    return -1*a;
+  }
+
+function map(arr, callback_fn){
+  let r=[];
+  for (let i=0; i<arr.length; i++){
+    r.push(callback_fn(arr[i]))
+  }  
+  return r;
+}
+
+map(a1,function(a){return a});
+
+const dune = ["paul", "gurney", "vladimir", "jessica", "chani"];
+
+map(dune,function(a){ return a})
+
+let a3 = [1, 2, 3, -9];
+
+function reduce(callback_fn,initialValue){
+  if (!this.length && initialValue === undefined){
+    throw TypeError("Reduce of empty array with no initial value");
+  }
+  let accumulator= initialValue;
+  let index =0;
+  if (initialValue === undefined){
+    accumulator = this[0];
+    index =1;
+  }
+  
+}
